e6211d940b0ff99b1ca269bc49fbd89567966ab0
Yan <crowbeat.47@gmail.com>
2025-06-09 15:21:14 +0500

    feat: some blocks footer and icons

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,6 +2,7 @@
 import { RouterLink } from 'vue-router'
 import Button from '@/components/UI/Button.vue'
 import { ref, onMounted, onBeforeUnmount, watch } from 'vue'
+import Icon from '@/components/Icons/Icon.vue'
 const modalIsActive = ref<boolean>(false)
 const modalState = ref<boolean>(false)
 const showBlackBar = ref(true)
@@ -43,9 +44,48 @@ onBeforeUnmount(() => window.removeEventListener('scroll', onScroll))
       </div>
     </header>
     <main class="pt-16 flex-1"><slot /></main>
-    <footer class="border-t border-gray-500">
+    <footer class="py-10">
       <div class="container">
-        <div class="flex items-center py-3 justify-between text-[12px]">
+        <div class="flex items-start justify-between gap-5 flex-wrap">
+          <div class="space-y-6 5">
+            <div class="flex items-end gap-4">
+              <img src="/logo.svg" alt="PotOfCast" class="h-10" />
+              <p class="text-[12px] font-medium text-gray-500">©2025.</p>
+            </div>
+            <div class="flex gap-5 items-center">
+              <a href="#" class="text-black hover:text-red-500"><Icon name="twitter" /></a>
+              <a href="#" class="text-black hover:text-red-500"><Icon name="instagram" /></a>
+              <a href="#" class="text-black hover:text-red-500"><Icon name="tiktok" /></a>
+            </div>
+          </div>
+          <nav class="flex items-start gap-20 flex-wrap">
+            <ul class="space-y-4 font-medium">
+              <li><a href="#" class="hover:text-red-500">About</a></li>
+              <li><a href="#" class="hover:text-red-500">Testimonials</a></li>
+              <li><a href="#" class="hover:text-red-500">Features</a></li>
+            </ul>
+            <ul class="space-y-4 font-medium">
+              <li><a href="#" class="hover:text-red-500">Episodes</a></li>
+              <li><a href="#" class="hover:text-red-500">Pricing</a></li>
+              <li><a href="#" class="hover:text-red-500">Blog</a></li>
+            </ul>
+          </nav>
+          <div>
+            <p class="text-gray-800">Listen to episodes on your fav platform:</p>
+            <div class="flex items-center flex-nowrap justify-start my-5">
+              <a href="#"><Icon name="google-text" class="h-6 w-full" /></a>
+              <a href="#"><Icon name="spotify-text" class="h-6" /></a>
+              <a href="#"><Icon name="youtube-text" class="h-5" /></a>
+            </div>
+
+            <p class="text-gray-800 mt-10">App available on:</p>
+            <div class="flex items-center flex-nowrap justify-start my-5 gap-5">
+              <a href="#"><Icon name="app-store" /></a>
+              <a href="#"><Icon name="google-play" /></a>
+            </div>
+          </div>
+        </div>
+        <div class="border-t border-gray-500 flex items-center py-3 justify-between text-[12px]">
           <p class="text-gray">
             ©2022. All Rights Reserved. <span class="text-red">Pod of Cast</span>
           </p>
